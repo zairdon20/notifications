e8ba6042930b3346fd0d9de27a0bb16f75fb620e
zairdon20 <josphat2020@gmail.com>
2023-10-14 00:00:06 +0200

    Fix styling

--- src/NotificattionPage.js
+++ src/NotificattionPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import  Names, { names }  from './Names';
 import images from './images/avatar-mark-webber.webp'
+import UnreadDot from './UnreadDot';
+
 
 
 function NotificattionPage() {
@@ -11,9 +13,11 @@ function NotificattionPage() {
         return <li key={index}>{modifiedName}</li>;
       });
     return (
-        <div className='flex  mt-6  space-x-2'>
-        <img src={images} alt="icon" />
-        <ul className=''><li className='font-bold text-blue-500 flex-col '>{modifiedNames[0]}</li><p className='text-base'>reacted to your recent post <strong>My first tournament today!</strong></p></ul>
+        
+        <div className=''>
+        
+        <ul className='flex bg-gray-200 m-2 border-b-6  border-y-8 mt-6  space-x-1 mr-2'><img src={images} className='w-8 h-8 ml-2' alt="icon" /><li className='font-bold text-blue-500 flex-col'>{modifiedNames[0]}</li><p className='text-base'>reacted to your recent post <strong>My first tournament today!<UnreadDot /></strong></p>
+        </ul> 
         <ul className='flex space-x-2'><li className='font-bold'>{modifiedNames[1]}</li> <p className='text-sm py-1 '>followed you</p></ul>
         <ul className='flex space-x-2'><li className='font-bold'>{modifiedNames[2]}reacted </li><p className='text-sm py-1 '>has joined your group Chess club</p> </ul>
         <ul className='flex space-x-2'><li className='font-bold'>{modifiedNames[3]}reacted </li><p className='text-sm py-1 '>reacted</p> </ul>
---
+++ src/UnreadDot.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UnreadDot = () => {
+    return <div className="w-3 h-3 bg-red-500 rounded-full inline-block ml-2  "></div>;
+  };
+  
+
+export default UnreadDot
